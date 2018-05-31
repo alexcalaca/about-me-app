0ebc9e73e796d1f78dd1e42a1caab05f0028630a
Alex Calaca <alexcalaca@gmail.com>
2018-05-31 01:18:08 +0000

    Create custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body
+  
+  def self.my
+    where(title: "My")
+  end
+  
+  def self.angular
+    where(subtitle: 'Angular')
+  end
 end
