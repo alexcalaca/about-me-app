1b46e3fb9db7f013fdc0125556def371508a2d93
Alex Calaca <alexcalaca@gmail.com>
2018-06-05 12:33:09 +0000

    Update guest_user for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-  
-  def current_user
-    super || OpenStruct.new(first_name: "Guest", last_name: "User", email: "example@test.com")
-  end
+  include CurrentUserConcern
 end
