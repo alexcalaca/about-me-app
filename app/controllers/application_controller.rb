e63a2bfdc26d8cfcfbf2faa123d30ae7beee86e9
Alex Calaca <alexcalaca@gmail.com>
2018-06-05 12:26:36 +0000

    Apply the object null pattern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  
+  def current_user
+    super || OpenStruct.new(first_name: "Guest", last_name: "User", email: "example@test.com")
+  end
 end
