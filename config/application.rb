167f361ddd9b0b5c0a1c28dc1db495c9324553e1
Alex Calaca <alexcalaca@gmail.com>
2018-06-05 00:22:15 +0000

    ;

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ module Workspace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.action_controller.permit_all_parameters = true
+    config.action_controller.permit_all_parameters = false
   end
 end
