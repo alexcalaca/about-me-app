0ca971d738b3e7d0eaf7afe52a761863b8e5e8ed
Alex Calaca <alexcalaca@gmail.com>
2018-06-05 00:23:02 +0000

    Set permit strong params to false

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ Bundler.require(*Rails.groups)
 module Workspace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
-    
     config.action_controller.permit_all_parameters = false
   end
 end
