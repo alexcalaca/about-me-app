f0da5b117e1dd62328f75b1e52e42c0a881ff94f
Alex Calaca <alexcalaca@gmail.com>
2018-06-19 16:34:29 +0000

    Add custom layout for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,50 +1,51 @@
 class PortfoliosController < ApplicationController
+  layout "portfolio"
 
-    def index
-      @portfolio_items = Portfolio.all
-    end
+  def index
+    @portfolio_items = Portfolio.all
+  end
+  
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+  
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
     
-    def angular
-      @angular_portfolio_items = Portfolio.angular
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was removed" }
     end
+  end
+  
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+  
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
     
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-    
-    def destroy
-      @portfolio_item = Portfolio.find(params[:id])
-      @portfolio_item.destroy
-      
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: "Portfolio was removed" }
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :new }
       end
     end
-    
-    def new
-      @portfolio_item = Portfolio.new
-      3.times { @portfolio_item.technologies.build }
-    end
-    
-    def create
-      @portfolio_item = Portfolio.new(portfolio_params)
-      
-      respond_to do |format|
-        if @portfolio_item.save
-          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-        else
-          format.html { render :new }
-        end
-      end
-    end
-    
-    private
-    
-    def portfolio_params
-      params.require(:portfolio).permit(:title,
-                                        :subtitle,
-                                        :body,
-                                        technologies_attributes: [:name]
-                                        )
-    end
+  end
+  
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                      )
+  end
 end 
